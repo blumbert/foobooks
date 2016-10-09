055cba77b4eccedaf3ae5e4298e4c28835515bce
B <B@Bs-MacBook.hsd1.ma.comcast.net>
2016-10-09 16:50:33 -0400

    added books controller and routes for books

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/books', 'BookController@index')->name('books.index');
+Route::get('/books/create', 'BookController@create')->name('books.create');
+Route::post('/books', 'BookController@store')->name('books.store');
+Route::get('/books/{book}', 'BookController@show')->name('books.show');
+Route::get('/books/{book}/edit', 'BookController@edit')->name('books.edit');
+Route::put('/books/{book}', 'BookController@update')->name('books.update');
+Route::delete('/books/{book}', 'BookController@destroy')->name('books.destroy');
